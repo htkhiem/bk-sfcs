b20571e592d18292f1135b46c7aa51c3daf3e4f6
Tuhuynhbk <tuntt2000@gmail.com>
2020-05-04 20:46:59 +0700

    Setters for QFood
    Add specific checks for Setters in Food.cpp

--- backend/BK-SFCS-Backend/src/food.cpp
+++ backend/BK-SFCS-Backend/src/food.cpp
@@ -15,24 +15,28 @@ QString QFood::getName()const{
     return this->name;
 };
 void QFood::setName(QString name){
-    this->name = name;
+   if (name == NULL) throw std::invalid_argument( "Name must not be blank" );
+   else this->name = name;
 };
 QString QFood::getType()const{
     return this->type;
 };
 void QFood::setType(QString type){
-    this->type = type;
+  if (type == NULL) throw std::invalid_argument( "Type must be within known ones" );
+  else  this->type = type;
 };
 QString QFood::getDescription()const{
     return this->description;
 };
 void QFood::setDescription(QString des){
-    this->description = des;
+  if (des == NULL) throw std::invalid_argument( "No description recieved" );
+   else this->description = des;
 };
 double QFood::getPrice()const{
     return this->price;
 };
 void QFood::setPrice(double price){
+  if (price <= 0) throw std::invalid_argument( "Price must not be negative or zero" );
     this->price = price;
 };
 
