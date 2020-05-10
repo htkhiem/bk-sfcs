c3c4bdebe5448d518c6bf6c6c5ef1246d8a070a4
htkhiem <42716602+htkhiem@users.noreply.github.com>
2020-05-10 17:34:38 +0700

    Backup

--- server/BK-SFCS-Stall/controller/stallmgmtcontroller.cpp
+++ server/BK-SFCS-Stall/controller/stallmgmtcontroller.cpp
@@ -19,12 +19,15 @@ void StallMgmtController::populateMgmtGraphs() {
 
 }
 bool StallMgmtController::loginAsManager(const QString& psw) {
-
+  if (current_stall.getMgmtPassword() != psw) return false;
+  return true;
 }
 void StallMgmtController::addFood(QFood * food) {
-
+  if (!food) throw invalid_argument("Food item pointer cannot be null.");
+  QFood new_food = *food;
+  // TODO
 }
-void StallMgmtController::editFood(QFood *food) {
+void StallMgmtController::editFood(QFood * food) {
 
 }
 bool StallMgmtController::removeFood(const QString& name) {
