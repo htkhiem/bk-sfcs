0007f119271fdd1ad444f5f15a2465dd88923e79
htkhiem <42716602+htkhiem@users.noreply.github.com>
2020-05-10 17:27:56 +0700

    Add framecode

---
+++ server/BK-SFCS-Stall/controller/stallmgmtcontroller.cpp
@@ -0,0 +1,32 @@
+#include "stallmgmtcontroller.h"
+
+StallMgmtController::StallMgmtController(QQmlApplicationEngine *eng, QObject *parent)
+  : AbstractController(eng, parent)
+{
+
+}
+
+bool StallMgmtController::login(const QString& name, const QString& psw) {
+
+}
+bool StallMgmtController::logout() {
+
+}
+void StallMgmtController::updateWaitlistViewModel() {
+
+}
+void StallMgmtController::populateMgmtGraphs() {
+
+}
+bool StallMgmtController::loginAsManager(const QString& psw) {
+
+}
+void StallMgmtController::addFood(QFood * food) {
+
+}
+void StallMgmtController::editFood(QFood *food) {
+
+}
+bool StallMgmtController::removeFood(const QString& name) {
+
+}
