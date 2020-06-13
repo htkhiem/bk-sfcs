0ee471e562def4f9ca2035691c78125896d1a41f
Huỳnh Thiện Khiêm <42716602+htkhiem@users.noreply.github.com>
2020-06-13 08:44:34 +0000

    Resolve conflict

--- backend/BK-SFCS-Backend/src/orderinfo.h
+++ backend/BK-SFCS-Backend/src/orderinfo.h
@@ -37,13 +37,9 @@ public:
   const QFood * getFoodItem() const;
   void setFoodItem(const QFood& _food);
   void setQuantity(int quantity);
-<<<<<<< HEAD
   QDateTime getReceived() const;
-=======
   void setSlipNumber(int _number);
   void setOrderID(QString _id);
-  QDateTime getReceived();
->>>>>>> 4778c87ba07fdb1776ffc405586cf50aa264a25a
   void setReceived();
   QDateTime getAnswered() const;
   void setAnswered(bool reject = false);
@@ -63,19 +59,6 @@ public:
    */
   void write(QJsonObject &json) const;
 
-
-  /**
-     * Read from a JSON object from disk.
-     * @param json JSON object to read from.
-     */
-    void readLog(const QJsonObject &json);
-    /**
-     * Write to a file on disk used stall_name as path.
-     * @param i: index of an order to write log.
-     * @param finished: boolen to set status of that order in log
-     */
-    void writeLog(int i,bool finished,const QString& stall_name) const;
-
   int getKiosk() const;
   void setKiosk(int value);
 
@@ -84,19 +67,11 @@ public:
 
 
 public slots:
-<<<<<<< HEAD
   int getQuantity() const;
   QString getItemName() const;
   double getTotal() const;
   QString getImageName() const;
-=======
-  int getQuantity();
-  int getSlipNumber();
   QString getOrderID();
-  QString getItemName();
-  double getTotal();
-  QUrl getImagePath(const QString& stall_name);
->>>>>>> 4778c87ba07fdb1776ffc405586cf50aa264a25a
 };
 
 #endif // ORDERINFO_H
