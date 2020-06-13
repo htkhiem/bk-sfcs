028cdbc0aab295570198bc2cc0f7098fb118fdfd
htkhiem <42716602+htkhiem@users.noreply.github.com>
2020-06-13 16:34:56 +0700

    Add missing order food item index assignment

--- kiosk/BK-SFCS/controller/kioskcontroller.cpp
+++ kiosk/BK-SFCS/controller/kioskcontroller.cpp
@@ -1,45 +1,47 @@
 #include "kioskcontroller.h"
 
 KioskController::KioskController(QQmlApplicationEngine *eng, QObject *parent)
-  : AbstractController(eng, "BK-SFCS Kiosk", parent)
+    : AbstractController(eng, "BK-SFCS Kiosk", parent)
 {
 }
 void KioskController::searchFilter(const QString& _input) {
-  for (auto ptr : menu_view_model) delete ptr;
-  menu_view_model.clear();
-  QVector<QFood>& temp = *(getCurrentStall()->getEditableMenu());
-  for (QFood& qfood : temp) {
-      if (categoryIsVisible(qfood.getType()) && qfood.getName().contains(_input)) {
-          QObject * qfoodptr = new QFood(qfood);
-          menu_view_model.append(qfoodptr);
+    for (auto ptr : menu_view_model) delete ptr;
+    menu_view_model.clear();
+    QVector<QFood>& temp = *(getCurrentStall()->getEditableMenu());
+    for (QFood& qfood : temp) {
+        if (categoryIsVisible(qfood.getType()) && qfood.getName().contains(_input)) {
+            QObject * qfoodptr = new QFood(qfood);
+            menu_view_model.append(qfoodptr);
         }
     }
-  p_engine->rootContext()->setContextProperty("menuViewModel", QVariant::fromValue(menu_view_model));
+    p_engine->rootContext()->setContextProperty("menuViewModel", QVariant::fromValue(menu_view_model));
 }
 void KioskController::setOrderFoodItem(int idx) {
-  if (idx < menu_view_model.size() && idx >= 0)
-    current_order.setFoodItem(*((QFood *) menu_view_model[idx]));
-  else throw range_error("Selected food item index out of range.");
+    if (idx < menu_view_model.size() && idx >= 0) {
+        current_order.setFoodItem(*((QFood *) menu_view_model[idx]));
+        current_order.setFoodIdx(idx);
+    }
+    else throw range_error("Selected food item index out of range.");
 }
 void KioskController::setOrderQuantity(int qty) {
-  if (qty > 0) current_order.setQuantity(qty);
-  else throw range_error("Ordering quantity less than one.");
+    if (qty > 0) current_order.setQuantity(qty);
+    else throw range_error("Ordering quantity less than one.");
 }
 void KioskController::setOrderMethod(int method) {
-  // TODO
+    // TODO
 }
 void KioskController::sendOrder() {
-  current_order.setStatus(waiting);
-  QJsonObject order_object;
-  current_order.write(order_object);
-  web_socket.sendTextMessage("OD " +
-                             QString::number(getClientIdx()) +
-                             " " + QString::number(getCurrentStallIdx()) +
-                             " " + QJsonDocument(order_object).toJson());
+    current_order.setStatus(waiting);
+    QJsonObject order_object;
+    current_order.write(order_object);
+    web_socket.sendTextMessage("OD " +
+                               QString::number(getClientIdx()) +
+                               " " + QString::number(getCurrentStallIdx()) +
+                               " " + QJsonDocument(order_object).toJson());
 }
 
 int KioskController::getCurrentOrderStatus() {
-  switch (current_order.getStatus()) {
+    switch (current_order.getStatus()) {
     case processing: return 1;
     case rejected: return 2;
     default: return 0;
@@ -47,14 +49,14 @@ int KioskController::getCurrentOrderStatus() {
 }
 
 void KioskController::parseRepliesToKiosk(const QString& message) {
-  QStringList response_tokens = message.split(' ', QString::SkipEmptyParts);
-  if (response_tokens[0] == "OK") current_order.setAnswered();
-  else {
-      current_order.setAnswered(true);
+    QStringList response_tokens = message.split(' ', QString::SkipEmptyParts);
+    if (response_tokens[0] == "OK") current_order.setAnswered();
+    else {
+        current_order.setAnswered(true);
     }
-  emit currentOrderStatusChanged();
+    emit currentOrderStatusChanged();
 }
 
 void KioskController::parseRepliesToStall(const QString &message) {
-  qDebug() << "[WARNING] Server sent stall-specific replies to this kiosk!";
+    qDebug() << "[WARNING] Server sent stall-specific replies to this kiosk!";
 }
